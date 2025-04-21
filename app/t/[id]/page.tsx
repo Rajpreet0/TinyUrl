a8d111c63575667267cc438a39c2cd4d7d84860a
Rajpreet0 <Rajpreet.Singh@gmx.de>
2025-04-21 23:28:39 +0200

    fix deployment

--- app/t/[id]/page.tsx
+++ app/t/[id]/page.tsx
@@ -1,9 +1,13 @@
 import { prisma } from "@/db/prisma";
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
+type Props = {
+    params: {
+      id: string;
+    };
+  };
 
-
-export default async function TinyRedirectPage({ params }: { params: { id: string } }) {
+export default async function TinyRedirectPage({ params }: Props) {
     const shortId = params.id;
 
     const entry = await prisma.urlMapper.findUnique({
@@ -11,11 +15,7 @@ export default async function TinyRedirectPage({ params }: { params: { id: strin
     });
 
     if (!entry) {
-        return (
-          <div className="w-full h-screen flex items-center justify-center">
-            <h1 className="text-2xl font-semibold text-red-500">404 - Link not found</h1>
-          </div>
-        );
+        notFound();
     }
 
     redirect(entry.siteUrl);
